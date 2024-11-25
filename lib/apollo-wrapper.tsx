c1ece8ed01b4565cfa5cdf8d871ad07edabfc828
trace2798 <113078518+trace2798@users.noreply.github.com>
2024-11-26 02:24:28 +0530

    removed public

--- lib/apollo-wrapper.tsx
+++ lib/apollo-wrapper.tsx
@@ -15,7 +15,7 @@ function makeClient() {
     uri: process.env.NEXT_PUBLIC_MODUS_API_BASE,
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.NEXT_PUBLIC_HYPERMODE_API_TOKEN}`,
+      Authorization: `Bearer ${process.env.HYPERMODE_API_TOKEN}`,
     },
     // you can disable result caching here if you want to
     // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
