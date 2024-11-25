54e9d6106e84b8262acbbd09478e7491a7c6e08b
trace2798 <113078518+trace2798@users.noreply.github.com>
2024-11-25 19:32:33 +0530

    added client side functionality

---
+++ components/get-quote-client.tsx
@@ -0,0 +1,76 @@
+"use client";
+import { FC, useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { Button, buttonVariants } from "./ui/button";
+import { GET_QUOTE } from "@/lib/queries";
+import { useLazyQuery, useSuspenseQuery } from "@apollo/client";
+import { cn } from "@/lib/utils";
+import { Skeleton } from "./ui/skeleton";
+
+interface GetQuoteClientProps {}
+
+const GetQuoteClient: FC<GetQuoteClientProps> = ({}) => {
+  const [quote, setQuote] = useState<string>("");
+  const [getQuote, { loading, error, data }] = useLazyQuery(GET_QUOTE, {
+    fetchPolicy: "no-cache",
+    onCompleted: (data) => {
+      console.log("Trivia DATA from LLM", data.getQuote);
+      setQuote(data.randomQuote.quote);
+    },
+  });
+  const handleQuoteClick = async () => {
+    getQuote();
+  };
+
+  console.log(data);
+
+  return (
+    <>
+      <div>
+        <Dialog>
+          <DialogTrigger>
+            <div
+              onClick={handleQuoteClick}
+              className={cn(buttonVariants({ variant: "outline" }))}
+            >
+              Get Client (client-side)
+            </div>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Random Quote</DialogTitle>
+              <DialogDescription>
+                A quote should be shown below and click on the Button below to
+                generate a new quote
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <div className=" items-center gap-4">
+                Quote: {quote || <Skeleton className="w-[200px] h-8" />}
+              </div>
+            </div>
+            <DialogFooter>
+              <Button
+                type="submit"
+                onClick={handleQuoteClick}
+                disabled={loading}
+              >
+                Regenerate Quote
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      </div>
+    </>
+  );
+};
+
+export default GetQuoteClient;
